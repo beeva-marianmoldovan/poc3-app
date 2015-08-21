fa234259aec2822817a1e239bd03426d5186544a
Marian C Moldovan <marian.claudiu@beeva.com>
2015-08-21 12:54:23 +0200

    Added Webview

--- app/src/main/java/com/beeva/iotday/MainActivity.java
+++ app/src/main/java/com/beeva/iotday/MainActivity.java
@@ -4,17 +4,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.beeva.ubiqlibrary.UbiqonsManager;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.webView)
+    WebView webView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         UbiqonsManager.register(this, "fac62835-9bx6-46e0-9d11-a7ac02929660", "xyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3JvbGUiOiJVc2VyIiwidXVpZCI6ImZhYzYyODM1LTliZTYtNDZlMC05ZDExLWE3YWMwMjkyOTY2MCIsImlhdCI6MTQ0MDA2MDczN30.8vFpAZWDP3drITO3OFY_Jclc0jHGgl2aa0srVTPeol4", "test_group", "test_user");
+        setUpWebView();
+
+    }
+
+    private void setUpWebView() {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.theapihour.com/");
     }
 
     @Override
